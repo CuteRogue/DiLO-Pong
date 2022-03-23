e8dc9074c2d87dbd40b07e9a41edb5d310bd9cbf
CuteRogue <andhika.rogue@gmail.com>
2022-03-23 23:45:29 +0700

    gerakin bola & padel
    nambah gerakan bola dan padel dengan input

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public int speed;
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        MoveObject(GetInput());
+    }
+
+    private Vector2 GetInput()
+    {
+        if (Input.GetKey(upKey))
+        {
+            return Vector2.up * speed;
+        }
+        else if (Input.GetKey(downKey))
+        {
+            return Vector2.down * speed;
+        }
+
+        return Vector2.zero;
+    }
+
+    private void MoveObject(Vector2 movement)
+    {
+        transform.Translate(movement * Time.deltaTime);
+    }
+}
